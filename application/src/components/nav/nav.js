e965e4b5c40d966262eee4c81e8d035db69cfed1
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2020-04-12 19:59:38 -0700

    feat(nav): add event listener that dispatches event to log out

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import { connect } from 'react-redux';
+import { logoutUser } from '../../redux/actions/authActions';
+
+const mapActionsToProps = dispatch => ({
+    commenceLogOut() {
+        dispatch(logoutUser())
+    }
+});
 
 const Nav = (props) => {
     return (
@@ -15,7 +23,11 @@ const Nav = (props) => {
                     <label className="nav-label">View Orders</label>
                 </div>
             </Link>
-            <Link to={"/login"} className="nav-link">
+            <Link
+                to={"/login"}
+                className="nav-link"
+                onClick={props.commenceLogOut}
+            >
                 <div className="nav-link-style">
                     <label className="nav-label">Log Out</label>
                 </div>
@@ -24,4 +36,4 @@ const Nav = (props) => {
     );
 }
 
-export default Nav;
+export default connect(null, mapActionsToProps)(Nav);
